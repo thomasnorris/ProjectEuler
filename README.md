2a0b000908fe031fec392e066cb2589db960c01f
thomasnorris <tom.norris135@gmail.com>
2018-06-25 15:55:27 -0400

    solved but could be more efficient

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Problem 4
+# Problem 4 - Solved
 - View the problem [here](https://projecteuler.net/problem=4).
--- program.js
+++ program.js
@@ -1,7 +1,9 @@
 (function() {
+    const MAX_THREE_DIGID_NUM = 999;
+
     var multiples = [];
-    for (var i = 1; i <= 99; i++) {
-        for (var j = 1; j <= 99; j++) {
+    for (var i = 1; i <= MAX_THREE_DIGID_NUM; i++) {
+        for (var j = 1; j <= MAX_THREE_DIGID_NUM; j++) {
             multiples.push(i * j);
         }
     }
@@ -9,7 +11,9 @@
     multiples = multiples.filter((el) => {
         el = el.toString();
         return el === el.split('').reverse().join(''); 
+    }).sort((a, b) => {
+        return a - b;
     });
 
-    console.log(multiples[multiples.length - 1]);
+    console.log('The largest palindromic number made from the product of two 3-digit numbers is: ' + multiples[multiples.length - 1]);
 })();
