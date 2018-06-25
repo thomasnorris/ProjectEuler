632fca165c8629a519ecfd053b218ba36042af84
thomasnorris <tom.norris135@gmail.com>
2018-06-25 15:48:14 -0400

    finds largest palindromic number from two digit dumbers

--- program.js
+++ program.js
@@ -1,3 +1,15 @@
 (function() {
-    // TODO: Add code
+    var multiples = [];
+    for (var i = 1; i <= 99; i++) {
+        for (var j = 1; j <= 99; j++) {
+            multiples.push(i * j);
+        }
+    }
+
+    multiples = multiples.filter((el) => {
+        el = el.toString();
+        return el === el.split('').reverse().join(''); 
+    });
+
+    console.log(multiples[multiples.length - 1]);
 })();
