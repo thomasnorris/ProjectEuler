05c06d0d02ca4d78bb2c8f322f322f60507c92ef
thomasnorris <tom.norris135@gmail.com>
2018-06-27 16:21:29 -0400

    example solved

--- program.js
+++ program.js
@@ -3,12 +3,20 @@
     // P(3, 14) is number of integers n from 1 < n < 14 where Streak(n) = 3;
     // P(3, 14) = 1. So there is 1 integer from 1 < n < 14 where Streak(n) = 3;
 
+    console.log(P(6, Math.pow(10, 6)));
+
     function P(s, N) {
+        var results = [];
         for (var i = 2; i < N; i++) {
             Streak((k) => {
-
+                results.push(k);
             }, i);
-        }
+        };
+        results = results.filter((el) => {
+            return el === s;
+        });
+
+        return results.length;
     }
 
     function Streak(callback, n, divisor = 1, count = 0) {
