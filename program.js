2aaad7575e24b4b05f836ec507109bfb4d648260
thomasnorris <tom.norris135@gmail.com>
2018-06-27 16:30:31 -0400

    might work but runs out of memory right now

--- program.js
+++ program.js
@@ -3,7 +3,13 @@
     // P(3, 14) is number of integers n from 1 < n < 14 where Streak(n) = 3;
     // P(3, 14) = 1. So there is 1 integer from 1 < n < 14 where Streak(n) = 3;
 
-    console.log(P(6, Math.pow(10, 6)));
+    var results = [];
+    for (var i = 2; i < 31; i++) {
+        results.push(P(i, Math.pow(4, i)));
+        console.log(results);
+    }
+
+    console.log(sum);
 
     function P(s, N) {
         var results = [];
@@ -17,16 +23,16 @@
         });
 
         return results.length;
-    }
 
-    function Streak(callback, n, divisor = 1, count = 0) {
-        if (n % divisor !== 0)
-            callback(count);
-        else {
-            ++n;
-            ++divisor;
-            ++count;
-            Streak(callback, n, divisor, count);
+        function Streak(callback, n, divisor = 1, count = 0) {
+            if (n % divisor !== 0)
+                callback(count);
+            else {
+                ++n;
+                ++divisor;
+                ++count;
+                Streak(callback, n, divisor, count);
+            }
         }
     }
 })();
