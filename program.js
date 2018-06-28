4b61692478a0b58214c67ebfcf76877b63d60744
thomasnorris <tom.norris135@gmail.com>
2018-06-28 08:33:43 -0400

    removed arrays; no longer runs out of memory

--- program.js
+++ program.js
@@ -3,26 +3,25 @@
     // P(3, 14) is number of integers n from 1 < n < 14 where Streak(n) = 3;
     // P(3, 14) = 1. So there is 1 integer from 1 < n < 14 where Streak(n) = 3;
 
-    var results = [];
-    for (var i = 2; i < 31; i++) {
-        results.push(P(i, Math.pow(4, i)));
-        console.log(results);
+    const RANGE_END = 31;
+    const RANGE_START = 1;
+
+    for (var i = RANGE_START; i <= RANGE_END; i++) {
+        P(i, Math.pow(4, i), (sum) => {
+            console.log(sum);
+        });
     }
 
-    console.log(sum);
-
-    function P(s, N) {
-        var results = [];
+    function P(s, N, callback) {
+        var sum = 0;
         for (var i = 2; i < N; i++) {
             Streak((k) => {
-                results.push(k);
+                if (k === s)
+                    ++sum;
             }, i);
         };
-        results = results.filter((el) => {
-            return el === s;
-        });
 
-        return results.length;
+        callback(sum);
 
         function Streak(callback, n, divisor = 1, count = 0) {
             if (n % divisor !== 0)
