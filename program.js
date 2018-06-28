f451bfdfe25adac4efb066e6cbd62bfdae688205
thomasnorris <tom.norris135@gmail.com>
2018-06-28 08:41:46 -0400

    sum is computed but takes a very long time

--- program.js
+++ program.js
@@ -3,25 +3,22 @@
     // P(3, 14) is number of integers n from 1 < n < 14 where Streak(n) = 3;
     // P(3, 14) = 1. So there is 1 integer from 1 < n < 14 where Streak(n) = 3;
 
-    const RANGE_END = 31;
-    const RANGE_START = 1;
-
-    for (var i = RANGE_START; i <= RANGE_END; i++) {
-        P(i, Math.pow(4, i), (sum) => {
-            console.log(sum);
-        });
+    var sum = 0;
+    for (var i = 1; i <= 31; i++) {
+        sum += P(i, Math.pow(4, i));
+        console.log('Sum after s = ' + i + ': ' + sum);
     }
 
-    function P(s, N, callback) {
-        var sum = 0;
+    function P(s, N) {
+        var results = 0;
         for (var i = 2; i < N; i++) {
             Streak((k) => {
                 if (k === s)
-                    ++sum;
+                    ++results;
             }, i);
         };
 
-        callback(sum);
+        return results;
 
         function Streak(callback, n, divisor = 1, count = 0) {
             if (n % divisor !== 0)
