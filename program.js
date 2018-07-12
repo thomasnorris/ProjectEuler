a5dc47b1439cfb6c9d1cbd16859567e5e1526d4d
thomasnorris <tom.norris135@gmail.com>
2018-07-12 14:33:46 -0400

    solved

--- program.js
+++ program.js
@@ -1,3 +1,26 @@
 (function() {
-    // TODO: Add code
+    var MAX = 10000000;
+    var _counter = 0;
+
+    for (var i = 1; i < MAX; i++) {
+        Calc(i);
+    }
+
+    console.log(_counter);
+
+    function Calc(num) {
+        if (num === 1 || num === 89) {
+            if (num === 89)
+                ++_counter;
+            return;
+        }
+
+        num = num.toString().split('');
+        var sum = 0;
+        for (var i = 0; i < num.length; i++) {
+            sum += Math.pow(parseInt(num[i]), 2);
+        }
+
+        Calc(sum);
+    }
 })();
