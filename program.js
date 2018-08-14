c41bf76b5506b66075997248c29b9e2b6f391455
Thomas Norris <tom.norris135@gmail.com>
2018-08-14 13:13:27 -0400

    moved 100 and 1000 counts because they are counted as hundred and thousand; some comments to explain what is happening; should now work up to 1000

--- program.js
+++ program.js
@@ -4,45 +4,57 @@
         4: [4, 5, 9],
         5: [3, 7, 8, 40, 50, 60],
         6: [11, 12, 20, 30, 80, 90],
-        7: [15, 16, 70],
-        8: [13, 14, 18, 19],
-        9: [17],
-        10: [100],
-        11: [1000]
+        7: [15, 16, 70, 100], // 100 = 'hundred'
+        8: [13, 14, 18, 19, 1000], // 1000 = 'thousand'
+        9: [17]
     }
 
-    const NUM_SUM = 100;
+    const NUM_SUM = 204;
     var sum = 0;
 
     for (var i = 1; i <= NUM_SUM; i++) {
-        if (GetLength(i) != '')
-            console.log(i);
-        else
-            Split(i, (pieces) => {
-                console.log(pieces);
-            });
+        Count(i, (pieces) => {
+            console.log(pieces);
+        });
     }
 
-    function Split(num, callback, pieces = []) {
+    function Count(num, callback, pieces = []) {
         var modulo;
         num.toString().length == 2 ? modulo = 10 : modulo = 100;
 
         var round = Math.floor(num / modulo) * modulo;
         var remainder = num - round;
-        if (round != 0)
-            pieces.push(round);
-        if (GetLength(remainder) == '')
-            Split(remainder, callback, pieces)
-        else {
-            pieces.push(remainder);
+
+        // if a single digit num OR the remainder is a multiple of the modulo
+        if (round != 0 || remainder == 0) {
+            if (GetLength(round) == '') {
+                pieces.push(round / modulo);
+                pieces.push(modulo);
+            } else {
+                pieces.push(round);
+            }
+
+            // if a hundred and x; eg: one two hundred and one
+            if (modulo == 100 && remainder != 0)
+                // 'and' keyword; eg: one hundred and twenty three
+                pieces.push('and');
+        }
+
+        // if the number isn't found in BASE_COUNTS AND there is a remainder
+        // split up the remainder
+        if (GetLength(remainder) == '' && remainder != 0) {
+            Count(remainder, callback, pieces)
+        } else {
+            if (remainder != 0)
+                pieces.push(remainder);
+            // callback
             callback(pieces);
         }
-    }
 
-    function GetLength(num) {
-        return Object.keys(BASE_COUNTS).filter((key) => {
-            return BASE_COUNTS[key].includes(num);
-        }).toString();
+        function GetLength(num) {
+            return Object.keys(BASE_COUNTS).filter((key) => {
+                return BASE_COUNTS[key].includes(num);
+            }).toString();
+        }
     }
-
 })();
