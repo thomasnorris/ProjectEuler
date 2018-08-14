8fb066dfc2279b5dc35d4fb4b6be2c599495c2a5
Thomas Norris <tom.norris135@gmail.com>
2018-08-14 13:28:59 -0400

    method renaming; constants for modulo values

--- program.js
+++ program.js
@@ -7,28 +7,33 @@
         7: [15, 16, 70, 100], // 100 = 'hundred'
         8: [13, 14, 18, 19, 1000], // 1000 = 'thousand'
         9: [17]
-    }
+    };
 
     // must be <= 1000 for proper functionality
     const NUM_SUM = 1000;
     var sum = 0;
 
-    for (var i = 1; i <= NUM_SUM; i++) {
-        Count(i, (pieces) => {
-            console.log(pieces);
-        });
-    }
+    BreakIntoPieces(342, (p) => {
+        console.log(p);
+    });
+    // for (var i = 1; i <= NUM_SUM; i++) {
+    //     BreakIntoPieces(i, (pieces) => {
+    //         console.log(pieces);
+    //     });
+    // }
 
-    function Count(num, callback, pieces = []) {
+    function BreakIntoPieces(num, callback, pieces = []) {
         var modulo;
-        num.toString().length == 2 ? modulo = 10 : modulo = 100;
+        var mod10 = 10;
+        var mod100 = 100;
+        num.toString().length == 2 ? modulo = mod10 : modulo = mod100;
 
         var round = Math.floor(num / modulo) * modulo;
         var remainder = num - round;
 
         // if a single digit num OR the remainder is a multiple of the modulo
         if (round != 0 || remainder == 0) {
-            if (GetLength(round) == '') {
+            if (modulo == mod100) {
                 pieces.push(round / modulo);
                 pieces.push(modulo);
             } else {
@@ -36,7 +41,7 @@
             }
 
             // if a hundred and x; eg: one two hundred and one
-            if (modulo == 100 && remainder != 0)
+            if (modulo == mod100 && remainder != 0)
                 // 'and' keyword; eg: one hundred and twenty three
                 pieces.push('and');
         }
@@ -44,7 +49,7 @@
         // if the number isn't found in BASE_COUNTS AND there is a remainder
         // split up the remainder
         if (GetLength(remainder) == '' && remainder != 0) {
-            Count(remainder, callback, pieces)
+            BreakIntoPieces(remainder, callback, pieces)
         } else {
             if (remainder != 0)
                 pieces.push(remainder);
