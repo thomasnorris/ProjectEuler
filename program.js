4bf82cdb8a587c80c41126875203abaf12595e6c
Thomas Norris <tom.norris135@gmail.com>
2018-08-14 10:35:27 -0400

    added base counts; logic for getting the length of the base counts

--- program.js
+++ program.js
@@ -1,3 +1,28 @@
 (function() {
-    // TODO: Add code
+    const BASE_COUNTS = {
+        3: [1, 2, 6, 10],
+        4: [4, 5, 9],
+        5: [3, 7, 8, 40, 50, 60],
+        6: [11, 12, 20, 30, 80, 80],
+        7: [15, 16, 70],
+        8: [13, 14, 18, 19],
+        9: [17],
+        10: [100],
+        11: [1000]
+    }
+
+    const NUM_SUM = 100;
+    var sum = 0;
+
+    for (var i = 1; i <= NUM_SUM; i++) {
+        if (GetLength(i) != '')
+            sum += i;
+    }
+
+    function GetLength(num) {
+        return Object.keys(BASE_COUNTS).filter((key) => {
+            return BASE_COUNTS[key].includes(num);
+        }).toString();
+    }
+
 })();
