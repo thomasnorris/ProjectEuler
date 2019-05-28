97283f394f4eaba4d5226bf19ebdf37398f4e70c
Thomas Norris <tom.norris135@gmail.com>
2019-05-28 10:22:19 -0400

    complete

--- program.js
+++ program.js
@@ -1,3 +1,30 @@
 (function() {
-    // TODO: Add code
+    const N = 600851475143;
+
+    var factors = PrimeFactorize(N);
+    console.log("The largest prime factor of \"" + N + "\" is: " + factors[factors.length - 1]);
+
+
+    function PrimeFactorize(n) {
+        var factors = [];
+        while (n % 2 === 0) {
+            factors.push(2);
+            n /= 2;
+        }
+
+        for (var i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i === 0) {
+                factors.push(i);
+                n /= i;
+            }
+        }
+
+        if (n > 2)
+            factors.push(n);
+
+        // Remove duplicates from factors
+        return factors.filter((v, i, a) => {
+            return a.indexOf(v) === i;
+        });
+    }
 })();
