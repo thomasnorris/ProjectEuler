0d47f766e7f48c54bd7e40aa8ca0c3f5fcefcfb9
Thomas Norris <tom.norris135@gmail.com>
2019-05-28 10:51:32 -0400

    solved

--- program.js
+++ program.js
@@ -1,3 +1,18 @@
 (function() {
-    // TODO: Add code
+    const UP_TO = 20;
+    var _num = 2520;
+
+    var found = false;
+    while (!found) {
+        for (var j = 1; j <= UP_TO; ++j) {
+            if (_num % j !== 0) {
+                ++_num;
+                break;
+            }
+            if (j === UP_TO)
+                found = true;
+        }
+    }
+
+    console.log("The smallest number that can evenly divide all numbers between 1 and " + UP_TO + " is: " + _num);
 })();
