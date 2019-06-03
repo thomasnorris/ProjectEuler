245a8eb8cd0b506e4de583a679fa63ceec72614c
Thomas Norris <tom.norris135@gmail.com>
2019-06-03 09:15:09 -0400

    functions for finding distinct primes and rad(n)

--- program.js
+++ program.js
@@ -1,3 +1,35 @@
 (function() {
-    // TODO: Add code
+
+    console.log(Rad(504));
+
+    function Rad(n) {
+        var rad = 1;
+        FindDistinctPrimes(n).forEach((prime) => {
+            rad *= prime;
+        });
+
+        return rad;
+    }
+
+    function FindDistinctPrimes(n) {
+        var factors = [];
+        while (n % 2 === 0)
+            CheckAndPush(2);
+
+        for (var i = 3; i <= Math.sqrt(n); ++i) {
+            while (n % i === 0)
+                CheckAndPush(i);
+        }
+
+        if (n > 2)
+            factors.push(n);
+
+        return factors;
+
+        function CheckAndPush(a) {
+            if (factors.indexOf(a) === -1)
+                factors.push(a);
+            n /= a;
+        }
+    }
 })();
