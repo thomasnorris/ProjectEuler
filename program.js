d2e08e5689e3db1cc2242945341b4099a6862500
Thomas Norris <tom.norris135@gmail.com>
2019-08-20 10:31:28 -0400

    solved

--- program.js
+++ program.js
@@ -1,3 +1,22 @@
 (function() {
-    // TODO: Add code
+    const SUM = 1000;
+
+    for (var a = 1; a <= SUM; ++a)
+        for (var b = 1; b <= SUM; ++b) {
+            var c = FindTriplet(a, b);
+            if (c && a + b + c === SUM)
+                console.log(a*b*c);
+        }
+
+    function FindTriplet(a, b) {
+        var c = Math.sqrt(Square(a) + Square(b));
+        if (Number.isInteger(c))
+            return c;
+
+            return false;
+
+        function Square(x) {
+            return Math.pow(x, 2);
+        }
+    }
 })();
