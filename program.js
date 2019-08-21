3a54ea8b95328850a88ed0e58d1f7cb63651c3fd
Thomas Norris <tom.norris135@gmail.com>
2019-08-21 10:36:52 -0400

    solved

--- program.js
+++ program.js
@@ -1,3 +1,21 @@
 (function() {
-    // TODO: Add code
+    const NTH_PRIME = 10001;
+
+    var i = 2;
+    var curr_prime = NTH_PRIME;
+    while (curr_prime) {
+        if (isPrime(i))
+            --curr_prime;
+        ++i;
+    }
+
+    console.log("The " + NTH_PRIME + " prime number is: " + --i);
+
+    function isPrime(x) {
+        for (var i = 2; i < x; ++i)
+            if (x % i === 0)
+                return false;
+
+        return x > 1;
+    }
 })();
