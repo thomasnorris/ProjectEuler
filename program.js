c70e8b56cdde40c012c31492443d4c4cdc55c552
Thomas Norris <tom.norris135@gmail.com>
2019-08-21 10:28:01 -0400

    solved

--- program.js
+++ program.js
@@ -1,3 +1,12 @@
 (function() {
-    // TODO: Add code
+    const MAX = 100;
+    var sumOfSquares = 0;
+    var squareOfSums = 0;
+
+    for (var i = 1; i <= MAX; ++i) {
+        sumOfSquares += Math.pow(i, 2);
+        squareOfSums += i;
+    }
+
+    console.log(Math.pow(squareOfSums, 2) - sumOfSquares);
 })();
